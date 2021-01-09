83d2a5c88831890054b22796070e9d9fd170270b
alizaynoune <simo.jokir@gmail.com>
2021-01-09 18:55:06 +0100

    first commit
    Signed-off-by: alizaynoune <simo.jokir@gmail.com>

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="UTF-8" />
+  <title>Camagru</title>
+  <link class="_css" rel="stylesheet" type="text/css" href="view/css/login.css" />
+	</head>
+	<body>
+        <nav>
+            <h1>Camagru</h1>
+            <h2>login</h2>
+        </nav>
+		<form action="create.php" method="POST">
+			<input type="text" placeholder="login or email" name="login"/></br>
+			<input class="password" type="password" placeholder="password" name="passwd"/></br>
+			<input class="submit" type="submit" name="submit" value="OK"/></br>
+		</form>
+        <a href="#"><p>forget password</p></a>
+	</body>
+</html>
