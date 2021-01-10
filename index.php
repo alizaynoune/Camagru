7e4612318d42e828a83fdeb9d89ca4be6f0398e7
alizaynoune <jokir.zaynoune@gmail.com>
2021-01-10 13:11:27 +0100

    add var colors
    Signed-off-by: alizaynoune <jokir.zaynoune@gmail.com>

--- index.php
+++ index.php
@@ -6,15 +6,19 @@
   <link class="_css" rel="stylesheet" type="text/css" href="view/css/login.css" />
 	</head>
 	<body>
-        <nav>
+        <nav class="navBar">
             <h1>Camagru</h1>
-            <h2>login</h2>
+            <h2>logUp</h2>
         </nav>
-		<form action="create.php" method="POST">
-			<input type="text" placeholder="login or email" name="login"/></br>
-			<input class="password" type="password" placeholder="password" name="passwd"/></br>
-			<input class="submit" type="submit" name="submit" value="OK"/></br>
+		<form class="form" action="login.php" method="POST">
+			<h1>Login</h1>
+			<input class="right" type="text" placeholder="username" name="login"/></br>
+			<input class="left" type="password" placeholder="       password" name="passwd"/></br>
+			<input class="submit right" type="submit" name="submit" value="OK"/></br>
+			<a class="logUpIn" href="#"><p>Create New Account</p></a>
+			<a class="forgetPass" href="#"><p>Forgotten password?</p></a>
+
 		</form>
-        <a href="#"><p>forget password</p></a>
+
 	</body>
 </html>
