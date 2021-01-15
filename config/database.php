69b98693e2e83772146b27115d8756d72d3303e4
alizaynoune <jokir.zaynoune@gmail.com>
2021-01-15 13:31:33 +0100

    add class connection
    Signed-off-by: alizaynoune <jokir.zaynoune@gmail.com>

--- config/database.php
+++ config/database.php
@@ -2,4 +2,20 @@
 	$DB_DNS = 'mysql:host=127.0.0.1';
 	$DB_USER = 'Camagru1337';
 	$DB_PASSWORD = 'ali';
+
+class DB_CONNECT {
+	protected	$DB_SERVER;
+	protected	$DB_DNS;
+	protected	$DB_USER;
+	protected	$DB_PASSWORD;
+	protected	$DB_NAME;
+
+	public function __construct(){
+		$this->DB_SERVER = 'mysql';
+		$this->DB_DNS = '127.0.0.1';
+		$this->DB_USER = 'Camagru1337';
+		$this->DB_PASSWORD = 'ali';
+		$this->DB_NAME = 'Camagru';
+	}
+}
 ?>
--- config/setup.php
+++ config/setup.php
@@ -1,6 +1,7 @@
 <?php
 include './database.php';
-
+$test = new DB_CONNECT;
+echo $test->DB_DNS;
 try {
     $dbh = new PDO($DB_DNS, $DB_USER, $DB_PASSWORD);
 } catch (PDOException $e) {
