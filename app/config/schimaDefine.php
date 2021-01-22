493672046c08510adfac54c98ba11b3755165cfe
alizaynoune <simo.jokir@gmail.com>
2021-01-22 18:23:56 +0100

    config create tables
    Signed-off-by: alizaynoune <simo.jokir@gmail.com>

--- app/config/schimaDefine.php
+++ app/config/schimaDefine.php
@@ -1,26 +1,28 @@
 <?php
-$DB_CREAT = array(
-    _table => "CREATE TABLE",
-    _database => "CREATE DATABASE",
-    _uid => "ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY",
-    _id => "",
-    _password => "",
-    _firstname => "VARCHAR(10) NOT NULL",
-    _lastname => "VARCAHR(10) NOT NULL",
-    _email => "VARCHAR(50) NOT NULL",
-    _string => "VARCHAR(120)",
-    _date => "DATETIME NOT NULL",
-    _active => "ENUMM (`true`, `false`)",
-    _notif => "ENUMM (`true`, `false`)"
-);
+// Name of tables
+define("_users_", "Users");
+define("_usersinfo_", "UsersInfo");
+define("_passwd_", "UserPasswd");
+define("_comment_", "UserComment");
+define("_posts_", "UserPosts");
+define("_Plike_", "PhotoLikes");
+define("_Clike_", "CommentLikes");
 
-$NAME_TABLE = array (
-    _users => "",
-    _usersinfo => "",
-    _userPasswd => "",
-    _comment => "",
-    _photos => "",
-    _posts => ""
+$DB_CREATE = array(
+    _table => " CREATE TABLE ",
+    _database => " CREATE DATABASE ",
+    _aid => " INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY ",
+    _id =>  " INT(6) UNSIGNED NOT NULL",
+    _idphoto => " INT(6) UNSIGNED NOT NULL DEFAULT 0",
+    _upwd => " upwd VARCHAR(100) NOT NULL ",
+    _login => " login VARCHAR(8) NOT NULL ",
+    _firstname => " firstname VARCHAR(10) NOT NULL ",
+    _lastname => " lastname VARCHAR(10) NOT NULL ",
+    _email => " email VARCHAR(50) NOT NULL ",
+    _string => " comment VARCHAR(120) ",
+    _date => " date DATETIME NOT NULL ",
+    _active => " active ENUM ('true', 'false') NOT NULL ",
+    _notif => " notif ENUMM (`true`, `false`) ",
 );
 
 $DB_GET = array (
--- app/config/setup.php
+++ app/config/setup.php
@@ -1,5 +1,6 @@
 <?php
-include './database.php';
+include 'database.php';
+require_once'schimaDefine.php';
 
 class	DB_SETUP extends DBClass{
 
@@ -12,15 +13,31 @@ class	DB_SETUP extends DBClass{
 		}
 	}
 
-	public function create_DB(){
+	private function create_DB(){
 		parent::Connect();
 		
 		self::DB_EXEC("DROP DATABASE IF EXISTS `$this->DB_NAME`");
 		self::DB_EXEC("CREATE DATABASE `$this->DB_NAME`");
 	}
+
+	public	function create_TB($ARRAY){
+		self::create_DB();
+		self::DB_EXEC("USE `$this->DB_NAME`");
+		self::DB_EXEC($ARRAY[_table]._users_.'
+			( id'.$ARRAY[_aid].','.$ARRAY[_login].','.$ARRAY[_active]
+			.','.$ARRAY[_email].')');
+		self::DB_EXEC($ARRAY[_table]._usersinfo_.'( idphoto'.
+			$ARRAY[_idphoto].', uid'. $ARRAY[_id]. ','.$ARRAY[_firstname].
+			','. $ARRAY[_lastname]. ','.$ARRAY[_email].')');
+		self::DB_EXEC($ARRAY[_table]._passwd_. '( uid'.$ARRAY[_id]. ','. $ARRAY[_upwd].')');
+		// self::DB_EXEC($ARRAY[_table]._posts_. '( pid'.$ARRAY[_aid]. ')');
+
+	}
 }
 
+
 $test = new DB_SETUP();
-echo $test->create_DB();
+echo $test->create_TB($DB_CREATE);
+// print_r ($DB_CREATE);
 
 ?>
