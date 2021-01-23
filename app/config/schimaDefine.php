612b5f6de1447c5b4a08501d88da66cf27298e55
alizaynoune <jokir.zaynoune@gmail.com>
2021-01-23 15:53:41 +0100

    creat db
    Signed-off-by: alizaynoune <jokir.zaynoune@gmail.com>

--- app/config/schimaDefine.php
+++ app/config/schimaDefine.php
@@ -3,48 +3,46 @@
 define("_users_", "Users");
 define("_usersinfo_", "UsersInfo");
 define("_passwd_", "UserPasswd");
-define("_comment_", "UserComment");
-define("_posts_", "UserPosts");
-define("_Plike_", "PhotoLikes");
+define("_comment_", "Comments");
+define("_posts_", "Posts");
+define("_Plike_", "PostLikes");
 define("_Clike_", "CommentLikes");
 
 $DB_CREATE = array(
-    _table => " CREATE TABLE ",
-    _database => " CREATE DATABASE ",
-    _aid => " INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY ",
-    _id =>  " INT(6) UNSIGNED NOT NULL",
-    _idphoto => " INT(6) UNSIGNED NOT NULL DEFAULT 0",
-    _upwd => " upwd VARCHAR(100) NOT NULL ",
-    _login => " login VARCHAR(8) NOT NULL ",
-    _firstname => " firstname VARCHAR(10) NOT NULL ",
-    _lastname => " lastname VARCHAR(10) NOT NULL ",
-    _email => " email VARCHAR(50) NOT NULL ",
-    _string => " comment VARCHAR(120) ",
-    _date => " date DATETIME NOT NULL ",
-    _active => " active ENUM ('true', 'false') NOT NULL ",
-    _notif => " notif ENUMM (`true`, `false`) ",
+    "_table"        => " CREATE TABLE ",
+    "_aid"          => "INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY",
+    "_id"           => "INT(6) UNSIGNED NOT NULL",
+    "_id_0"         => "INT(6) UNSIGNED NOT NULL DEFAULT 0",
+    "_login"        => "VARCHAR(8) NOT NULL",
+    "_name"         => "VARCHAR(10) NOT NULL",
+    "_email"        => "email VARCHAR(20) NOT NULL",
+    "_pwd"          => "pwd VARCHAR(100) NOT NULL",
+    "_string"       => "VARCHAR(200) NOT NULL",
+    "_comment"      => "VARCHAR(255) NOT NULL",
+    "_date"         => "DATETIME NOT NULL",
+    "_true_false"   => "ENUM ('true', 'false') NOT NULL",
 );
 
 $DB_GET = array (
-    _login => "",
-    _passowrd => "",
-    _email => "",
-    _userinfo => "",
-    _photo => "",
-    _photoProfile => "",
-    _comment => "",
-    _allposts => "",
-    _sedNotif => "",
-    _activeNotif => ""
+    "_login"        => "",
+    "_passowrd"     => "",
+    "_email"        => "",
+    "_userinfo"     => "",
+    "_photo"        => "",
+    "_photoProfile" => "",
+    "_comment"      => "",
+    "_allposts"     => "",
+    "_sedNotif"     => "",
+    "_activeNotif"  => ""
 );
 
 $DB_INSERT = array(
-    _user => "",
-    _photoProfile => "",
-    _photo => "",
-    _comment => "",
-    _activeEmail => "",
-    _changePasswd => ""
+    "_user"         => "",
+    "_photoProfile" => "",
+    "_photo"        => "",
+    "_comment"      => "",
+    "_activeEmail"  => "",
+    "_changePasswd" => ""
 );
 
 ?>
--- app/config/setup.php
+++ app/config/setup.php
@@ -23,14 +23,13 @@ class	DB_SETUP extends DBClass{
 	public	function create_TB($ARRAY){
 		self::create_DB();
 		self::DB_EXEC("USE `$this->DB_NAME`");
-		self::DB_EXEC($ARRAY[_table]._users_.'
-			( id'.$ARRAY[_aid].','.$ARRAY[_login].','.$ARRAY[_active]
-			.','.$ARRAY[_email].')');
-		self::DB_EXEC($ARRAY[_table]._usersinfo_.'( idphoto'.
-			$ARRAY[_idphoto].', uid'. $ARRAY[_id]. ','.$ARRAY[_firstname].
-			','. $ARRAY[_lastname]. ','.$ARRAY[_email].')');
-		self::DB_EXEC($ARRAY[_table]._passwd_. '( uid'.$ARRAY[_id]. ','. $ARRAY[_upwd].')');
-		// self::DB_EXEC($ARRAY[_table]._posts_. '( pid'.$ARRAY[_aid]. ')');
+		self::DB_EXEC($ARRAY["_table"]._users_.' ( id '.$ARRAY["_aid"].', login '.$ARRAY["_login"].' , active '.$ARRAY["_true_false"].' , dateCreate '.$ARRAY["_date"].')');
+		self::DB_EXEC($ARRAY["_table"]._usersinfo_.' ( uid '.$ARRAY["_id"].' , photid '. $ARRAY["_id_0"]. ' , firstname '.$ARRAY["_name"].' , lastname '. $ARRAY["_name"]. ' , '.$ARRAY["_email"].')');
+		self::DB_EXEC($ARRAY["_table"]._passwd_. ' ( uid '.$ARRAY["_id"]. ' , '. $ARRAY["_pwd"].')');
+		self::DB_EXEC($ARRAY["_table"]._posts_. ' ( pid '.$ARRAY["_aid"] . ' , uid ' . $ARRAY['_id'] . ' , pLink ' . $ARRAY['_string'] . ' , likes ' . $ARRAY['_id_0'] . ' , dateCreate ' . $ARRAY['_date'] .')');
+		self::DB_EXEC($ARRAY["_table"]._comment_. '( cid '.$ARRAY['_aid'] . ' , uid ' . $ARRAY['_id'] . ' , pid ' . $ARRAY['_id'] . ' , Comment ' . $ARRAY['_comment'] . ' , likes '. $ARRAY['_id_0'] .' , dateCreate ' . $ARRAY['_date'] .')');
+		self::DB_EXEC($ARRAY["_table"]._Plike_. '( like_id '. $ARRAY['_aid'] . ' , post_id '. $ARRAY['_id'] . ' , uid ' . $ARRAY['_id'] . ' , dateCreate ' . $ARRAY['_date'] .')');
+		self::DB_EXEC($ARRAY["_table"]._Clike_. '( like_id '. $ARRAY['_aid'] . ' , comment_id '. $ARRAY['_id'] . ' , uid ' . $ARRAY['_id'] . ' , dateCreate ' . $ARRAY['_date'] .')');
 
 	}
 }
