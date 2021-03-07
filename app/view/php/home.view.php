ac2e738f2a483356fbb40b13458ee6c9bf05d3cc
alizaynoune <simo.jokir@gmail.com>
2021-03-07 18:59:51 +0100

    style post not finesh yet
    Signed-off-by: alizaynoune <simo.jokir@gmail.com>

--- app/view/php/home.view.php
+++ app/view/php/home.view.php
@@ -19,63 +19,75 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/app/model/class.model.php';
 	<?php require_once $_SERVER['DOCUMENT_ROOT'].'/app/view/php/menu.view.php';?>
 	<div class="content">
 		<h1>home</h1>
-
-
 		<div class="post">
-			<h2 class=""
 			<span class="delet_post"></span>
-			<h3 class="title">title test</h3>
-			<img src="../../../public/icone/profile.jpg">
+			<input type="text" name='hash'>
+			<div class="info">
+				<img id="img_owner" src="../../../public/icone/profile.jpg">
+				<h4 class="owner">name of owner</h4>
+				<h2 class="title">title test</h2>
+			</div>
+			
+			<img id='img_post' src="../../../public/icone/profile.jpg">
+			
 			<div class="comment_like">
 				<div class="contener_like">
-					<span class="like"></span>
-					<h4 class="likeNbr">1likes</h4>
+					<span class="like">20</span>
+					<!-- <h4 class="likeNbr">1likes</h4> -->
+					<h4 class="commentNbr">10 comments</h4>
 				</div>
-				<div class="new_comment">
-						<input type="text" name="comment" placeholder="add comment" />
-						<input type="submit" name="submit" value="comment"/>
-				</div>
+
+			
+			
 				<div class="contener_comment">
-						<h4 class="commentNbr">10 comments</h4>
-						<div class="comment">
-							<div class="old_comment">
-								<span class='delet_comment'></span>
-								<p>comment for test</p>
-							</div>
-							<div class="old_comment">
-								<span class='delet_comment'></span>
-								<p>comment for sjfdljkdsfljkdlskfjklsdjfljks;l test</p>
-							</div>
-							<div class="old_comment">
-								<span class='delet_comment'></span>
-								<p>comment for test</p>
-							</div>
-							<div class="old_comment">
-								<span class='delet_comment'></span>
-								<p>comment for test</p>
-							</div>
-							<div class="old_comment">
-								<span class='delet_comment'></span>
-								<p>comment for test</p>
-							</div>
-							<div class="old_comment">
-								<span class='delet_comment'></span>
-								<p>comment for test</p>
-							</div>
-							<div class="old_comment">
-								<span class='delet_comment'></span>
-								<p>comment for test</p>
-							</div>
-							<div class="old_comment">
-								<span class='delet_comment'></span>
-								<p>comment for test</p>
-							</div>
-							<div class="old_comment">
-								<span class='delet_comment'></span>
-								<p>comment for test</p>
-							</div>
+					
+					<div class="comment">
+						<div class="old_comment">
+						<span class='dislike'>20</span>
+						<span class='delet_comment'></span>
+							<p>comment for test</p>
+							<p>10-25-2021</p>
+							
 						</div>
+						<div class="old_comment">
+						<span class='like'>0</span>
+							<p>comment for test</p>
+							<p>10-25-2021</p>
+							<span class='delet_comment'></span>
+						</div>
+						<div class="old_comment">
+						<span class='like'>0</span>
+							<p>comment for test</p>
+							<p>10-25-2021</p>
+							<span class='delet_comment'></span>
+						</div>
+						<div class="old_comment">
+						<span class='like'>0</span>
+							<p>comment ffsjldskfjklj fjs dfjfklsj fs jflkj fsjklj;fs jlfkslaj jsdkflkjs jdsfklj or test</p>
+							<p>10-25-2021</p>
+							<span class='delet_comment'></span>
+						</div>
+						<div class="old_comment">
+						<span class='like'>0</span>
+							<p>comment for test</p>
+							<p>10-25-2021</p>
+							<span class='delet_comment'></span>
+						</div>
+						<div class="old_comment">
+						<span class='like'>0</span>
+							<p>comment for test</p>
+							<p>10-25-2021</p>
+							<span class='delet_comment'></span>
+						</div>
+					</div>
+			</div>
+			<div class="new_comment">
+				<input type="text" name="comment" placeholder="add comment" />
+				<div>
+					<input id="submit_comment" type="submit" name="submit" value="comment"/>
+					<label id='submit' for="submit_comment" ></label>
 				</div>
+			</div>
 			</div>
 		</div>
 	</div>
